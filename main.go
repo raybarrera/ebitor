470664233d66a3acfa6987daa4293d16d917af47
Ray Barrera <ray@raybarrera.com>
2023-10-12 20:59:35 -0700

    Hello, world.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"golang.org/x/image/font"
@@ -14,8 +14,28 @@ var (
 )
 
 func init() {
+	uiImage = ebiten.NewImage(1, 1)
+	uiImage.Fill(color.White)
+}
+
+type Editor struct{}
+
+func (e *Editor) Update() error {
+	return nil
+}
+
+func (e *Editor) Draw(screen *ebiten.Image) {
+	screen.DrawImage(uiImage, nil)
+}
+
+func (e *Editor) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 320, 240
 }
 
 func main() {
-	fmt.Printf("This is a test %v", "test")
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Ebitengine Editor")
+	if err := ebiten.RunGame(&Editor{}); err != nil {
+		panic(err)
+	}
 }
